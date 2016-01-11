c357e54bfb60fdf29eafa72f962ac6d72b213487
Rikard Braathen <rikard.braathen@gmail.com>
2016-01-11 14:04:24 +0100

    Changed download method

--- assets/refresh-data.ps1
+++ assets/refresh-data.ps1
@@ -10,7 +10,7 @@ if ($PSVersionTable.PSVersion.Major -lt 3) {
 }
 
 Try {
-    Write-Host "`nDownloading data..."
+    Write-Host -NoNewline "`nDownloading data"
     New-Item -ItemType Directory -Force -Path "$current\data" | Out-Null
 
     $wc = New-Object System.Net.WebClient
@@ -19,13 +19,17 @@ Try {
 	# Data configuration
     $data = "var sizingData = " + ($wc.DownloadString($config_data) | ConvertFrom-Csv -Delimiter ',' | ConvertTo-Json -Compress | Out-String)
 	Out-File -inputobject $data -filepath "$current/data/config_data.js"
-
+    Write-Host -NoNewline "."
+    
     $lang = ($wc.DownloadString($config_language) | ConvertFrom-Csv -Delimiter ',')
+    Write-Host -NoNewline "."
 
     Foreach ($l in $lang)
     {
         If ($l.Url -ne "")
         {
+            Write-Host -NoNewline "."
+
             $a = $wc.DownloadString($l.Url + "?embedded=true")
 
             $a = $a -replace '(?s)^.*</style>|<div id="footer">.*$', ''
@@ -35,8 +39,11 @@ Try {
             $a = $a -replace '(?s)^.*</title>', ''
 
             # clean up c2 c3 etc - causes a conflict with variables for some reason...
-#            ([regex]'(?s) class=\".*?\"').Matches($a) | ForEach-Object {Write-Host $_ }
+            #([regex]'(?s) class=\".*?\"').Matches($a) | ForEach-Object {Write-Host $_ }
             #([regex]'(?s) class=\"c.*?\"').Matches($a) | ForEach-Object { $a = $a.Replace($_,'') }
+
+            # fix spaces for Word (not finished)
+            #([regex]'(?s) <.*?>').Matches($a) | ForEach-Object { $a = $a.Replace($_,'') }
 
             # remove spans
             ([regex]'(?s)<span.*?>|</span>').Matches($a) | ForEach-Object { $a = $a.Replace($_,'') }
@@ -52,7 +59,9 @@ Try {
             {
                 $tmpname = $i.substring($i.length - 15, 15)
                 $a = $a -replace $i, "assets/data/$tmpname"
-                Invoke-WebRequest -Uri $i -UseBasicParsing -OutFile "$current\data\$tmpname"
+                $wc.DownloadFile($i, "$current\data\$tmpname")
+                #Invoke-WebRequest -Uri $i -UseBasicParsing -OutFile "$current\data\$tmpname"
+                Write-Host -NoNewline "."
             }
 
             $l.Text = $a
@@ -65,7 +74,7 @@ Try {
     $b = $lang | ConvertTo-Json -Compress | Out-String
     $b = 'var languageData = ' + $b
     Out-File -inputobject $b -filepath "$current/data/text.js"
-
+    Write-Host
 }
 Catch [system.exception] {
     Write-Host "`nERROR:" $_.Exception.Message "`n"
--- update.ps1
+++ update.ps1
@@ -6,6 +6,9 @@ if ($PSVersionTable.PSVersion.Major -lt 3) {
     return
 }
 
+$wc = New-Object System.Net.WebClient
+$wc.Encoding = [System.Text.Encoding]::UTF8
+
 Write-Host "Updating application..."
 
 If (Test-Path "$current\assets") {
@@ -18,7 +21,7 @@ If (Test-Path "$current\update.ps1") {
     Remove-Item -Recurse -Force "$current\update.ps1" | Out-Null
 }
 
-Invoke-WebRequest -Uri "https://github.com/braathen/qlik-hardware-configurator/archive/master.zip" -UseBasicParsing -OutFile "./master.zip"
+$wc.DownloadFile("https://bit.ly/qhc-update", "$current\master.zip")
 
 Add-Type -assembly "system.io.compression.filesystem"
 
@@ -26,7 +29,9 @@ Add-Type -assembly "system.io.compression.filesystem"
 
 Move-Item "$current/qlik-hardware-configurator-master/*" -Force
 
-Remove-Item -Recurse -Force "$current\.gitignore"
+If (Test-Path "$current\.gitignore") {
+    Remove-Item -Recurse -Force "$current\.gitignore"
+}
 
 Remove-Item -Recurse -Force "$current\qlik-hardware-configurator-master"
 
