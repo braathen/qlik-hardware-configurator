e00384df43ceb33642dd7929c5acf806941ea4b3
Rikard Braathen <rikard.braathen@gmail.com>
2016-02-12 11:59:11 +0100

    fix

--- assets/js/hwsizing.js
+++ assets/js/hwsizing.js
@@ -162,18 +162,18 @@ function getText(tags) {
 
     // get text for language
     var x = _.find(languageData, function(item) {
-        console.log(navigator.language.toLowerCase());
-        var y = item.Language == navigator.language.toLowerCase() && compareArrays(item.Tags, tags);
-        console.log(y);
-        if (y !== undefined)
-            return y;
+        //console.log(navigator.language.toLowerCase());
+        // navigator.language.toLowerCase()
+        //var y = item.Language == 'en-us' && compareArrays(item.Tags, tags);
+        //if (y !== undefined)
+        //    return y;
         return item.Language == 'en-us' && compareArrays(item.Tags, tags);
     });
 
     // no match
     if(x == undefined)
     {
-        console.log(tags);
+        //console.log(tags);
         return "[NOT IMPLEMENTED YET]";
     }
 
